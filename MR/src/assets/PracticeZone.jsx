5eb37eeb3e376dd49ed44fdc2d1686d12420653e
cameronmcnz <you@example.com>
2024-08-30 11:06:42 -0400

    'd'

--- MR/src/assets/PracticeZone.jsx
+++ MR/src/assets/PracticeZone.jsx
@@ -24,3 +24,42 @@ public class DwarfMiner {
         System.out.println("Dwarf Miner ASCII art inside comments.");
     }
 }
+<!--
+
+
+                                __________            .###.
+                                |         \   .###.  ##'  `#
+                                |_________/  ##' `####'       .##
+                                    \  \     ##----,_        ##
+                                     \  \  /        ###.    ,##
+                                      \  \/  O u O  | ##._.###
+                                       \_ \       ,-.| `###'
+                                      /  ) \    _/  |
+                                      |  |  \       |
+                                       \  \_ \      \_____
+                                        \/  ) \_/    \_   \
+                                        |  /\ (___/       |
+                                        \    \  \         |
+                                         \    \_/         |
+                                         |    _         /
+                                        /   _/ \    /  |
+                                      __|__/_\_/\__/\_/________________
+                 ____________________|_________________________________|
+                <___               | |                        __/
+                    \____          | |                      _/
+                         \____     | |                    _/
+                              \____| |    ===========    /
+                                   \_|     W O R L D    /
+                                     |     A N V I L    |
+                                     |    ===========    \
+                                    _/      Light up      \_
+                                 __/       the forge!       \__
+                                /            _______           \
+                                #           /       \           #
+                                ############         ############
+                                                        World Anvil created  Dimitris
+                                                         Artwork by TJ Trewin
+                                                        Loved by All the Anvilites
+        -->
+
+// lol this is bad why isnt it commenting out???
